bcfd40f32befeb06a3101eaa40c68e961a5d86f7
R. Tyler Croy <rtyler@brokenco.de>
2019-11-24 10:05:24 -0800

    Checkpointing a simple hack to get static assets served from Rocket
    The Template support in Rocket doesn't _appear_ to support just passing through
    `Strign` or `str`, so I think I will eschew server-side templating and just
    require JavaScript here.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[macro_use]
+extern crate rust_embed;
+
+use rocket_contrib::templates::Template;
+
+
+#[derive(RustEmbed)]
+#[folder="views"]
+struct Views;
+
+#[get("/")]
+fn index() -> String {
+    let view = Views::get("index.html.hbs").unwrap();
+    String::from_utf8_lossy(view.as_ref()).into_owned()
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .attach(Template::fairing())
+        .mount("/", routes![index]).launch();
 }
