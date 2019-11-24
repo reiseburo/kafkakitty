2a070cfa576d1bb871c82762fcf690e351486fa1
R. Tyler Croy <rtyler@brokenco.de>
2019-11-24 10:41:31 -0800

    Incorporate some example code from the websocket crate for websockets
    Since the Rocket websocket support (in the async branch) is not yet ready, I
    don't want to build on top of it. I should be able to work with the websocket
    thread in this way and send messages from a Kafka thread into the websocket
    threads without too much issue

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@
 
 #[macro_use]
 extern crate rocket;
-
+extern crate websocket;
 #[macro_use]
 extern crate rust_embed;
 
-use rocket_contrib::templates::Template;
-
+use std::thread;
+use websocket::sync::Server;
+use websocket::OwnedMessage;
 
 #[derive(RustEmbed)]
 #[folder="views"]
@@ -19,8 +20,56 @@ fn index() -> String {
     String::from_utf8_lossy(view.as_ref()).into_owned()
 }
 
+fn websocket() {
+    let server = Server::bind("127.0.0.1:8001").unwrap();
+
+    for request in server.filter_map(Result::ok) {
+        // Spawn a new thread for each connection.
+        thread::spawn(|| {
+            if !request.protocols().contains(&"rust-websocket".to_string()) {
+                request.reject().unwrap();
+                return;
+            }
+
+            let mut client = request.use_protocol("rust-websocket").accept().unwrap();
+
+            let ip = client.peer_addr().unwrap();
+
+            println!("Connection from {}", ip);
+
+            let message = OwnedMessage::Text("Hello".to_string());
+            client.send_message(&message).unwrap();
+
+            let (mut receiver, mut sender) = client.split().unwrap();
+
+            for message in receiver.incoming_messages() {
+                let message = message.unwrap();
+
+                match message {
+                    OwnedMessage::Close(_) => {
+                        let message = OwnedMessage::Close(None);
+                        sender.send_message(&message).unwrap();
+                        println!("Client {} disconnected", ip);
+                        return;
+                    }
+                    OwnedMessage::Ping(ping) => {
+                        let message = OwnedMessage::Pong(ping);
+                        sender.send_message(&message).unwrap();
+                    }
+                    _ => sender.send_message(&message).unwrap(),
+                }
+            }
+        });
+    }
+
+}
+
 fn main() {
+
+    thread::spawn(|| {
+        websocket()
+    });
+
     rocket::ignite()
-        .attach(Template::fairing())
         .mount("/", routes![index]).launch();
 }
