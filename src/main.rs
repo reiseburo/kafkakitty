31826d6bf8027761145d501e9b7bf63973542768
R. Tyler Croy <rtyler@brokenco.de>
2019-11-28 11:41:07 -0800

    A terrible work in progress that at least passes messages over the websocket
    This is still heavily relying on smashing Strings around between threads via the
    crossbeam channels.
    
    This needs to be reworked tremendously, but gotta checkpoint again at a working
    state before I gut it :smile_cat:

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+extern crate crossbeam;
+extern crate rdkafka;
 #[macro_use]
 extern crate rocket;
-extern crate tungstenite;
 #[macro_use]
 extern crate rust_embed;
+extern crate tungstenite;
+
+use crossbeam::channel::{Sender, Receiver};
+
+use futures::stream::Stream;
+
+use rdkafka::client::ClientContext;
+use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
+use rdkafka::consumer::stream_consumer::StreamConsumer;
+use rdkafka::consumer::{CommitMode, Consumer, ConsumerContext};
+use rdkafka::Message;
+use rdkafka::message::Headers;
 
 use rocket::http::{ContentType, Status};
 use rocket::response;
+
 use std::ffi::OsStr;
 use std::io::Cursor;
 use std::net::TcpListener;
 use std::path::PathBuf;
+use std::sync::mpsc;
 use std::thread;
 use std::thread::spawn;
+use std::time::Duration;
+
 use tungstenite::server::accept;
 
 #[derive(RustEmbed)]
@@ -45,30 +62,93 @@ fn assets<'r>(file: PathBuf) -> response::Result<'r> {
   )
 }
 
-fn websocket() {
+fn websocket(rx: Receiver<String>) {
     /// A WebSocket echo server
     let server = TcpListener::bind("127.0.0.1:8001").unwrap();
     for stream in server.incoming() {
+        let channel = rx.clone();
         println!("Connection..");
         spawn (move || {
             let mut websocket = accept(stream.unwrap()).unwrap();
             loop {
-                let msg = websocket.read_message().unwrap();
-
-                // We do not want to send back ping/pong messages.
-                if msg.is_binary() || msg.is_text() {
-                    websocket.write_message(msg).unwrap();
-                }
+                let msg = channel.recv().unwrap();
+                websocket.write_message(tungstenite::Message::Text(msg)).unwrap();
             }
         });
     }
 }
 
 fn main() {
-    thread::spawn(|| {
-        websocket()
+    let (sender, receiver) = crossbeam::channel::unbounded::<String>();
+    thread::spawn(move || {
+        websocket(receiver);
+    });
+
+    thread::spawn(move || {
+        consume_and_print("localhost:9092", "kafkakitty", &["test"], sender);
     });
 
     rocket::ignite()
         .mount("/", routes![index, assets]).launch();
+}
+
+struct LoggingConsumerContext;
+
+impl ClientContext for LoggingConsumerContext {}
+
+impl ConsumerContext for LoggingConsumerContext {
+}
+
+// Define a new type for convenience
+type LoggingConsumer = StreamConsumer<LoggingConsumerContext>;
+
+fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str], sender: Sender<String>) {
+    let context = LoggingConsumerContext;
+
+    let consumer: LoggingConsumer = ClientConfig::new()
+        .set("group.id", group_id)
+        .set("bootstrap.servers", brokers)
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "true")
+        //.set("statistics.interval.ms", "30000")
+        //.set("auto.offset.reset", "smallest")
+        .set_log_level(RDKafkaLogLevel::Debug)
+        .create_with_context(context)
+        .expect("Consumer creation failed");
+
+    consumer.subscribe(&topics.to_vec())
+        .expect("Can't subscribe to specified topics");
+
+    // consumer.start() returns a stream. The stream can be used ot chain together expensive steps,
+    // such as complex computations on a thread pool or asynchronous IO.
+    let message_stream = consumer.start();
+
+    for message in message_stream.wait() {
+        match message {
+            Err(_) => println!("Error while reading from stream."),
+            Ok(Err(e)) => println!("Kafka error: {}", e),
+            Ok(Ok(m)) => {
+                let payload = match m.payload_view::<str>() {
+                    None => "",
+                    Some(Ok(s)) => s,
+                    Some(Err(e)) => {
+                        println!("Error while deserializing message payload: {:?}", e);
+                        ""
+                    }
+                };
+                println!("key: '{:?}', payload: '{}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
+                      m.key(), payload, m.topic(), m.partition(), m.offset(), m.timestamp());
+                sender.send(payload.to_string()).unwrap();
+
+                if let Some(headers) = m.headers() {
+                    for i in 0..headers.count() {
+                        let header = headers.get(i).unwrap();
+                        println!("  Header {:#?}: {:?}", header.0, header.1);
+                    }
+                }
+                consumer.commit_message(&m, CommitMode::Async).unwrap();
+            }
+        };
+    }
 }
