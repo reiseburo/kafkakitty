3775ffb92b39758fb8f07ba4bedf643ce6be992e
R. Tyler Croy <rtyler@brokenco.de>
2019-11-28 12:22:43 -0800

    Add command-line arguments roughly matching what kafkacat has

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,15 @@ extern crate rocket;
 extern crate rust_embed;
 extern crate tungstenite;
 
+use clap::{App, Arg};
 use crossbeam::channel::{Sender, Receiver};
-
 use futures::stream::Stream;
 
-use rdkafka::client::ClientContext;
-use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
+use log::{info, warn};
+
+use rdkafka::config::ClientConfig;
 use rdkafka::consumer::stream_consumer::StreamConsumer;
-use rdkafka::consumer::{CommitMode, Consumer, ConsumerContext};
+use rdkafka::consumer::{CommitMode, Consumer};
 use rdkafka::Message;
 use rdkafka::message::Headers;
 
@@ -26,10 +27,8 @@ use std::ffi::OsStr;
 use std::io::Cursor;
 use std::net::TcpListener;
 use std::path::PathBuf;
-use std::sync::mpsc;
 use std::thread;
 use std::thread::spawn;
-use std::time::Duration;
 
 use tungstenite::server::accept;
 
@@ -63,7 +62,7 @@ fn assets<'r>(file: PathBuf) -> response::Result<'r> {
 }
 
 fn websocket(rx: Receiver<String>) {
-    /// A WebSocket echo server
+    // A WebSocket echo server
     let server = TcpListener::bind("127.0.0.1:8001").unwrap();
     for stream in server.incoming() {
         let channel = rx.clone();
@@ -79,43 +78,92 @@ fn websocket(rx: Receiver<String>) {
 }
 
 fn main() {
+    let matches = App::new("Kafkakitty 😿")
+        .version(option_env!("CARGO_PKG_VERSION").unwrap_or(""))
+        .about("A cute little Kafka consumer")
+        .arg(Arg::with_name("brokers")
+             .short("b")
+             .long("brokers")
+             .help("Broker list in kafka format")
+             .takes_value(true)
+             .default_value("localhost:9092"))
+        .arg(Arg::with_name("group-id")
+             .short("G")
+             .long("group-id")
+             .help("Consumer group ID for Kafka")
+             .takes_value(true)
+             .default_value("kafkakitty"))
+        .arg(Arg::with_name("topics")
+             .short("t")
+             .long("topics")
+             .help("List of topics to follow")
+             .takes_value(true)
+             .multiple(true)
+             .required(true))
+        .arg(Arg::with_name("no-open")
+             .short("n")
+             .long("no-open")
+             .help("Disable opening a browser window automatically")
+             .takes_value(false))
+        .get_matches();
+
     let (sender, receiver) = crossbeam::channel::unbounded::<String>();
+
+    /*
+     * Kafkakitty requires at minimum three threads:
+     *   - The websocket server thread
+     *   - The Rocket server thread
+     *   - The Kafka consumer thread
+     *
+     * There's communication between the websocket and consumer thread via channels
+     * provided by the crossbeam crate.
+     *
+     * This allows multiple browser windows to receive the same messages
+     */
     thread::spawn(move || {
         websocket(receiver);
     });
 
     thread::spawn(move || {
-        consume_and_print("localhost:9092", "kafkakitty", &["test"], sender);
+        let topics = matches.values_of("topics").unwrap().collect::<Vec<&str>>();
+        let brokers = matches.value_of("brokers").unwrap();
+        let group_id = matches.value_of("group-id").unwrap();
+        info!("Setting up consumer for {}", brokers);
+
+        consume(sender, brokers, group_id, &topics);
     });
 
+    // Launch the web app
     rocket::ignite()
         .mount("/", routes![index, assets]).launch();
 }
 
-fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str], sender: Sender<String>) {
+/**
+ * The consume function is solely responsible for consuming from the given Kafka
+ * topics and sending the message over the channel via the `tx` Sender
+ *
+ */
+fn consume(tx: Sender<String>,
+           brokers: &str,
+           group_id: &str,
+           topics: &[&str]) {
+
     let consumer: StreamConsumer = ClientConfig::new()
         .set("group.id", group_id)
         .set("bootstrap.servers", brokers)
         .set("enable.partition.eof", "false")
         .set("session.timeout.ms", "6000")
         .set("enable.auto.commit", "true")
-        //.set("statistics.interval.ms", "30000")
-        //.set("auto.offset.reset", "smallest")
-        .set_log_level(RDKafkaLogLevel::Debug)
         .create()
         .expect("Consumer creation failed");
 
     consumer.subscribe(&topics.to_vec())
         .expect("Can't subscribe to specified topics");
 
-    // consumer.start() returns a stream. The stream can be used ot chain together expensive steps,
-    // such as complex computations on a thread pool or asynchronous IO.
-    let message_stream = consumer.start();
-
-    for message in message_stream.wait() {
+    for message in consumer.start().wait() {
         match message {
-            Err(_) => println!("Error while reading from stream."),
-            Ok(Err(e)) => println!("Kafka error: {}", e),
+            Err(_) => warn!("Error while reading from stream."),
+            Ok(Err(e)) => warn!("Kafka error: {}", e),
             Ok(Ok(m)) => {
                 let payload = match m.payload_view::<str>() {
                     None => "",
@@ -125,14 +173,14 @@ fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str], sender: Sen
                         ""
                     }
                 };
-                println!("key: '{:?}', payload: '{}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
+                info!("key: '{:?}', payload: '{}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
                       m.key(), payload, m.topic(), m.partition(), m.offset(), m.timestamp());
-                sender.send(payload.to_string()).unwrap();
+                tx.send(payload.to_string()).unwrap();
 
                 if let Some(headers) = m.headers() {
                     for i in 0..headers.count() {
                         let header = headers.get(i).unwrap();
-                        println!("  Header {:#?}: {:?}", header.0, header.1);
+                        info!("  Header {:#?}: {:?}", header.0, header.1);
                     }
                 }
                 consumer.commit_message(&m, CommitMode::Async).unwrap();
