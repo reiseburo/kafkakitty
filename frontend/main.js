65bdd2b6ea56131de8a2cfdb797d7776df3474b3
R. Tyler Croy <rtyler@brokenco.de>
2019-11-28 12:53:10 -0800

    Restructure the frontend a wee bit to allow for a reconnecting websocket
    The Vue Hello World is still in place, slowly replacing..

--- frontend/main.js
+++ frontend/main.js
@@ -5,6 +5,34 @@ import App from './App'
 
 Vue.config.productionTip = false
 
+console.log('Kafkakitty bootstrapping..')
+
+function wsConnect () {
+  const socket = new WebSocket('ws://localhost:8001')
+  socket.onopen = (event) => {
+    console.log('Kafkakitty connected! 😺')
+  }
+  socket.onerror = (err) => {
+    console.error('😿 Kafkakitty encountered an error:', err)
+    socket.close()
+    wsConnect()
+  }
+  socket.onclose = (event) => {
+    console.log('Kafkakitty connection lost, retrying..')
+    wsConnect()
+  }
+
+  socket.onmessage = (event) => {
+    console.log(`Received: ${event.data}`)
+    const container = document.getElementById('app')
+    const d = document.createElement('div')
+    d.className = 'row'
+    d.innerHTML = `<pre><code>${event.data}</code></pre>`
+    container.insertBefore(d, container.firstChild)
+  }
+}
+wsConnect()
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
